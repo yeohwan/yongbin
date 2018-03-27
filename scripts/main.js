1112526e1eb9920e65677293aa4f5f25bde552d2
yeohwanyun <cwss1503@gmail.com>
2018-03-27 11:57:16 +0900

    second commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,23 @@
-// Image switcher code
-
-var myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-	var mySrc = myImage.getAttribute('src');
-	if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/firefox2.png');
-	} else {
-	  myImage.setAttribute ('src','images/firefox-icon.png');
-	}
-}
-
-// Personalized welcome message code
-
-var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
-
-function setUserName() {
-  var myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+myHeading.innerHTML = 'Cloud Service<br>2018-1';
+function getIdPw() {
+    var id = prompt('ID 입력', '');
+    alert(id + '가 로그인함.');
+    var passwordSystem = '12345';
+    var password = prompt('PW 입력', '');
+    if (password === passwordSystem) alert('정상 로그인!');
+    else alert('비밀번호 오류');
 }
 
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  var storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+document.querySelector('h1').onclick = function () {
+    getIdPw();
 }
 
-myButton.onclick = function() {
-  setUserName();
+var myImg = document.querySelector('img');
+myImg.onclick = function () {
+    var src = myImg.getAttribute('src');
+    if (src === 'images/firefox-icon.png')
+        myImg.setAttribute('src', 'images/mokwon.png');
+    else
+        myImg.setAttribute('src', 'images/firefox-icon.png');
 }
